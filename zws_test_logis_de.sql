a323c3551c654b029627c8da0c90cbbd43960571
Xiang Fu <XFu@intl.isdgroup.com>
2019-06-05 11:56:49 +0200

    添加新表对应国际铁路段和ups德国境内段

---
+++ zws_test_logis_de.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 05. Jun 2019 um 11:54
+-- Server Version: 5.6.14
+-- PHP-Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `zhongw_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `zws_test_logis_de`
+--
+
+CREATE TABLE IF NOT EXISTS `zws_test_logis_de` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `de_packet_id` varchar(50) NOT NULL DEFAULT '',
+  `railway_id` mediumint(8) unsigned NOT NULL,
+  `de_log` text NOT NULL,
+  `de_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `de_status` tinyint(2) NOT NULL DEFAULT '0',
+  `de_company` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `railway_id` (`railway_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+--
+-- Daten für Tabelle `zws_test_logis_de`
+--
+
+INSERT INTO `zws_test_logis_de` (`id`, `de_packet_id`, `railway_id`, `de_log`, `de_time`, `de_status`, `de_company`) VALUES
+(1, '1Z30YE216852356434', 1, '', '2019-06-05 09:27:43', 0, 'UPS'),
+(2, '1Z30YE216852937217', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(3, '1Z30YE216853460444', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(4, '1Z30YE216854334025', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(5, '1Z30YE216852397873', 2, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(6, '1Z30YE216854385060', 2, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(7, '1Z30YE216852376574', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(8, '1Z30YE216853408608', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(9, '1Z30YE216852558994', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(10, '1Z30YE216852486982', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
+(11, '1Z30YE216852011814', 3, '', '2019-06-05 09:32:16', 0, 'UPS');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ zws_test_railway_inter.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 05. Jun 2019 um 11:53
+-- Server Version: 5.6.14
+-- PHP-Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `zhongw_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `zws_test_railway_inter`
+--
+
+CREATE TABLE IF NOT EXISTS `zws_test_railway_inter` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `railway_id` varchar(50) NOT NULL DEFAULT '',
+  `inter_log` text NOT NULL,
+  `inter_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `inter_status` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Daten für Tabelle `zws_test_railway_inter`
+--
+
+INSERT INTO `zws_test_railway_inter` (`id`, `railway_id`, `inter_log`, `inter_time`, `inter_status`) VALUES
+(1, 'V0320221078', '', '0000-00-00 00:00:00', 0),
+(2, 'V0320221149', '', '2019-06-05 09:04:21', 0),
+(3, 'V0320222362', '', '2019-06-05 09:05:42', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
