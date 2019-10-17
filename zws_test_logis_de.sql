b6cb980861b5436381067c94aaf20d934b02a2a5
Xiang Fu <XFu@isdgroup.com>
2019-10-17 16:08:04 +0200

    mysql

--- zws_test_logis_de.sql
+++ zws_test_logis_de.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `zws_test_logis_de` (
   `railway_id` mediumint(8) unsigned NOT NULL,
   `de_log` text NOT NULL,
   `de_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `de_status` tinyint(2) NOT NULL DEFAULT '0',
+  `de_status` tinyint(2) NOT NULL DEFAULT '-1',
   `de_company` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `railway_id` (`railway_id`)
@@ -43,17 +43,17 @@ CREATE TABLE IF NOT EXISTS `zws_test_logis_de` (
 --
 
 INSERT INTO `zws_test_logis_de` (`id`, `de_packet_sn`, `railway_id`, `de_log`, `de_time`, `de_status`, `de_company`) VALUES
-(1, '1Z30YE216852356434', 1, '', '2019-06-05 09:27:43', 0, 'UPS'),
-(2, '1Z30YE216852937217', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(3, '1Z30YE216853460444', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(4, '1Z30YE216854334025', 1, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(5, '1Z30YE216852397873', 2, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(6, '1Z30YE216854385060', 2, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(7, '1Z30YE216852376574', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(8, '1Z30YE216853408608', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(9, '1Z30YE216852558994', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(10, '1Z30YE216852486982', 3, '', '2019-06-05 09:32:16', 0, 'UPS'),
-(11, '1Z30YE216852011814', 3, '', '2019-06-05 09:32:16', 0, 'UPS');
+(1, '1Z30YE216852356434', 1, '', '2019-06-05 09:27:43', -1, 'ups'),
+(2, '1Z30YE216852937217', 1, '', '2019-06-05 09:32:16', -1, 'ups'),
+(3, '1Z30YE216853460444', 1, '', '2019-06-05 09:32:16', -1, 'ups'),
+(4, '1Z30YE216854334025', 1, '', '2019-06-05 09:32:16', -1, 'ups'),
+(5, '1Z30YE216852397873', 2, '', '2019-06-05 09:32:16', -1, 'ups'),
+(6, '1Z30YE216854385060', 2, '', '2019-06-05 09:32:16', -1, 'ups'),
+(7, '1Z30YE216852376574', 3, '', '2019-06-05 09:32:16', -1, 'ups'),
+(8, '1Z30YE216853408608', 3, '', '2019-06-05 09:32:16', -1, 'ups'),
+(9, '1Z30YE216852558994', 3, '', '2019-06-05 09:32:16', -1, 'ups'),
+(10, '1Z30YE216852486982', 3, '', '2019-06-05 09:32:16', -1, 'ups'),
+(11, '1Z30YE216852011814', 3, '', '2019-06-05 09:32:16', -1, 'ups');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- zws_test_railway_inter.sql
+++ zws_test_railway_inter.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 05. Jun 2019 um 15:46
+-- Erstellungszeit: 17. Okt 2019 um 15:58
 -- Server Version: 5.6.14
 -- PHP-Version: 5.5.6
 
@@ -31,7 +31,8 @@ CREATE TABLE IF NOT EXISTS `zws_test_railway_inter` (
   `railway_sn` varchar(50) NOT NULL DEFAULT '',
   `inter_log` text NOT NULL,
   `inter_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `inter_status` tinyint(2) NOT NULL DEFAULT '0',
+  `inter_status` tinyint(2) NOT NULL DEFAULT '-1',
+  `inter_company` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
@@ -39,10 +40,10 @@ CREATE TABLE IF NOT EXISTS `zws_test_railway_inter` (
 -- Daten für Tabelle `zws_test_railway_inter`
 --
 
-INSERT INTO `zws_test_railway_inter` (`id`, `railway_sn`, `inter_log`, `inter_time`, `inter_status`) VALUES
-(1, 'V0320221078', '', '0000-00-00 00:00:00', 0),
-(2, 'V0320221149', '', '2019-06-05 09:04:21', 0),
-(3, 'V0320222362', '', '2019-06-05 09:05:42', 0);
+INSERT INTO `zws_test_railway_inter` (`id`, `railway_sn`, `inter_log`, `inter_time`, `inter_status`, `inter_company`) VALUES
+(1, 'V0320221078', '', '2019-10-17 13:35:38', -1, ''),
+(2, 'V0320221149', '', '2019-06-05 09:04:21', -1, ''),
+(3, 'V0320222362', '', '2019-06-05 09:05:42', -1, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
